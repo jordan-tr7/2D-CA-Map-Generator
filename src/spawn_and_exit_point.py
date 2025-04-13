0a97f5d05f903a4900c04dfdadd638e47ea33d6a
Tristan Jordan <jordan.tr@northeastern.edu>
2025-04-13 16:04:32 -0400

    starting bfs

--- src/spawn_and_exit_point.py
+++ src/spawn_and_exit_point.py
@@ -2,7 +2,7 @@
 import argparse
 from helpers import map_helpers as maps
 import numpy as np # can be del once migrated
-
+import queue
 
 def create_index_matrix(grid):
     table_shape = grid.shape
@@ -18,7 +18,6 @@ def create_index_matrix(grid):
             index += 1
 
     return(new_mat)
-
 
 
 def create_adjacency_matrix(grid):
@@ -52,6 +51,53 @@ def create_adjacency_matrix(grid):
 
 
 
+def find_rooms(game_map, adjacency_matrix, index_matrix):
+
+    map_dim = game_map.shape 
+    num_rows = map_dim[0]
+    num_cols = map_dim[1]
+
+    visited_matrix = np.zeros(shape=(num_rows, num_cols), dtype = int)
+
+    for row in range(num_rows):
+        for col in range(num_cols):
+
+            print(f"Reviewing df[{row}, {col}] out of [{num_rows}, {num_cols}]")
+
+            current_cell = index_matrix[row, col]
+            cell_value = game_map[row, col]
+
+            print(f"Cell Index: {current_cell}, Cell Value: {cell_value}")
+
+            # if our current cell is either blank, an item, or enemy, we can consider that
+            # part of a room, here we want to start a BFS to fully explore the room
+            if cell_value in [0, 2, 3]:
+                
+                q = queue.Queue()
+                q.put(current_cell) # add the current cell's index to queue
+
+                print(q)
+
+                while q.qsize() > 0:
+                    
+                    print(q.qsize())
+
+                    curr = q.get() 
+
+                    print(f"Current in queue: {curr}")
+                    # find neighbors of current element
+                    neighbors = [i for i, value in enumerate(adjacency_matrix[curr]) if value == 1]
+                    print(neighbors)
+
+                    for neighbor in neighbors:
+
+                        # check if visited, only add if not visited AND a valid open cell
+                        if (visited_matrix[neighbor // num_rows, neighbor % num_cols] == 0) and (game_map[neighbor // num_rows, neighbor % num_cols] in [0, 2, 3]):
+                            q.put(neighbor)
+
+                    # outside of for loop, change value of visited cell
+                    game_map[row, col] = 420
+                    visited_matrix[row, col] = 1
 
 
 
@@ -83,8 +129,8 @@ def main():
 
     modified_map = maps.add_detail(new_map, args.prob_item, args.prob_enemy)
     
-    modified_map[0,0] = 69
-    print(modified_map[0, 0])    
+    #modified_map[0,0] = 69
+    #print(modified_map[0, 0])    
     maps.plot_complex_grid(modified_map, f"spawn-points/Test-{test_no}_Base-Map")
 
     
@@ -92,9 +138,28 @@ def main():
 
     ind_mat = results[0]
     adj_mat = results[1]
-    print(ind_mat)
-    print(adj_mat)
+    #print(ind_mat)
+    #print(ind_mat.shape)
+    #print(adj_mat)
+    #print(adj_mat.shape)
+    #print([i for i, value in enumerate(adj_mat[101]) if value == 1]) # find the cell values that are linked to given cell
+    
+    #res = [i for i, value in enumerate(adj_mat[101]) if value == 1]
 
+    #for item in res:
+    #    print(f"The cell ({item // 100}, {item % 100}) neighbors 0, 0")
+
+
+
+    find_rooms(modified_map, adj_mat, ind_mat)
+    maps.plot_complex_grid(modified_map, f"spawn-points/Test-{test_no}_Base-Map_After-Room-Finding")
+    
+    #print(9999 // 100)
+    #print(9999 % 100)
+    #print(103 // 100)
+    #print(103 % 100)
+    #print(9950 // 100)
+    #print(9950 % 100)
 
 
 if __name__ == "__main__":
